a6f25b12fe6f518dddf81cb23e2438535b99cad5
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-08-31 21:21:52 +0800

    test

--- Petition/Startup.cs
+++ Petition/Startup.cs
@@ -38,7 +38,7 @@ namespace Petition
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) // test
             {
                 app.UseDeveloperExceptionPage();
             }
