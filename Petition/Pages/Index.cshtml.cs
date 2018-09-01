7484b3b5aea73e1a6c88f966c57dbfcc4de6a194
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-09-02 05:35:01 +0800

    almost there

--- Petition/Pages/Index.cshtml.cs
+++ Petition/Pages/Index.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Petition.Data;
+using Petition.Models;
 
 namespace Petition.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public PetitionContext _context { get; set; }
+        public List<PetitionModel> PetitionList { get; set; }
 
+        public IndexModel(PetitionContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGetAsync()
+        {
+            PetitionList = _context.PetitionModel.ToList();
+            return Page();
         }
     }
 }
