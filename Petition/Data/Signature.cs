503e02cb5319fe64c9053eae0822393455caaf1c
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-09-01 19:43:44 +0800

    done sum shit

---
+++ Petition/Data/Signature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Petition.Data
+{
+    public class Signature
+    {
+        public string SignatureId { get; set; }
+        public PetitionModel Petition { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Petitioner Signer { get; set; }
+        public string SignatureDetails { get; set; }
+        public int Traction { get; set; }
+    }
+}
---
+++ Petition/Migrations/20180901111202_PetitionSignature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Petition.Migrations
+{
+    public partial class PetitionSignature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetitionModel",
+                columns: table => new
+                {
+                    PetitionModelId = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    Photo = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Votes = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetitionModel", x => x.PetitionModelId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetitionModel");
+        }
+    }
+}
